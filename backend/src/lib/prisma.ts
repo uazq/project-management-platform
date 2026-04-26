f68475f89a71670a029cb1def780279feb5f1b4b
abodesamer <abodesamer123@gmail.com>
2026-04-26 23:28:10 +0300

    Use @prisma/client directly, remove custom output path

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,5 +1,10 @@
-import { PrismaClient } from '../generated/prisma';
+/*import { PrismaClient } from '../generated/prisma';
 
 const prisma = new PrismaClient();
 
+export default prisma;*/
+
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 export default prisma;
